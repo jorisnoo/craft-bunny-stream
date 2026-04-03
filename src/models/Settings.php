52288e3b38d881bb360ea37ee05a1eb0a5db391b
Joris Noordermeer <joris@noordermeer.ch>
2026-04-03 20:46:47 +0800

    feat: load bunny stream settings from environment variables in init method

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -3,6 +3,7 @@
 namespace Noo\CraftBunnyStream\models;
 
 use craft\base\Model;
+use craft\helpers\App;
 
 class Settings extends Model
 {
@@ -10,4 +11,14 @@ class Settings extends Model
     public ?string $bunnyStreamLibraryId = null;
     public ?string $bunnyStreamCdnHostname = null;
     public ?string $bunnyStreamCollectionId = null;
+
+    public function init(): void
+    {
+        parent::init();
+
+        $this->bunnyStreamAccessKey ??= App::env('BUNNY_STREAM_ACCESS_KEY');
+        $this->bunnyStreamLibraryId ??= App::env('BUNNY_STREAM_LIBRARY_ID');
+        $this->bunnyStreamCdnHostname ??= App::env('BUNNY_STREAM_CDN_HOSTNAME');
+        $this->bunnyStreamCollectionId ??= App::env('BUNNY_STREAM_COLLECTION_ID');
+    }
 }
